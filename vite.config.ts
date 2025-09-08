399c723e80c69fc59f565f6fbd21b029543478cd
马玉棚 <2836365261@qq.com>
2025-09-08 20:46:58 +0800

    打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,25 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name][extname]',
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          antd: ['antd'],
+          'antd-icons': ['@ant-design/icons'],
+          markdown: ['react-markdown', 'remark-gfm'],
+          'syntax-highlighter': ['react-syntax-highlighter'],
+          vendor: ['@ant-design/x', 'lodash', 'dayjs']
+        }
+      }
+    }
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
