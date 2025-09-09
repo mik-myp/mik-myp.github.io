6c2846138abd5985a403750872728b1eff03879d
马玉棚 <2836365261@qq.com>
2025-09-09 12:38:44 +0800

    将new-blog打包后的静态资源提交上去

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import router from './router';
-import { RouterProvider } from 'react-router';
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
-export default App;
--- src/store/index.ts
+++
@@ -1,4 +0,0 @@
-// 导出所有store模块
-export * from './createStore';
-export * from './reactbits';
-export * from './global';
