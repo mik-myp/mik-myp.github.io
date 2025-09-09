e1e9639cd1f749baef20784ce27b44a9090391bf
马玉棚 <2836365261@qq.com>
2025-09-09 12:30:34 +0800

    回退部分代码

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/mik-myp.github.io/' // 替换为你的仓库名
-      : '/',
   build: {
     outDir: 'dist',
     rollupOptions: {
